7a672fec84d124deac673d400d4d36ef3d2c5310
Arman <rmanaem@protonmail.ch>
2022-11-01 15:31:58 -0400

    Implemented input fields

--- vue/nb-query-mock/src/App.vue
+++ vue/nb-query-mock/src/App.vue
@@ -20,7 +20,62 @@
             <form method="post">
               <div class="form-row row">
                 <div class="form-group col-md-6"></div>
+                <label class="form-label">Age lower</label>
+                <input
+                  type="number"
+                  name="age_lower"
+                  step="any"
+                  class="numberinput form-control"
+                />
                 <div class="form-group col-md-6"></div>
+                <label class="form-label">Age upper</label>
+                <input
+                  type="number"
+                  name="age_upper"
+                  step="any"
+                  class="numberinput form-control"
+                />
+              </div>
+              <div>
+                <label class="form-label">Sex</label>
+                <select type="sex" class="select form-select">
+                  <option value selected>All</option>
+                  <option>female</option>
+                  <option>male</option>
+                </select>
+              </div>
+              <div>
+                <label class="form-label">Modality</label>
+                <select type="modality" class="select form-select">
+                  <option>Diffusion weighted</option>
+                  <option>EEG</option>
+                  <option>Flow weighted</option>
+                  <option>T1 weighted</option>
+                  <option>T2 weighted</option>
+                </select>
+              </div>
+              <div>
+                <label class="form-label">Diagnosis</label>
+                <select name="diagnosis" class="select form-select">
+                  <option>All</option>
+                  <option>Parkinson's disease</option>
+                </select>
+              </div>
+              <div>
+                <label class="form-label">Is control</label>
+                <select name="is_control" class="select form-select">
+                  <option>All</option>
+                  <option>control</option>
+                </select>
+              </div>
+              <div>
+                <label class="form-label">Num sessions</label>
+                <input
+                  type="numebr"
+                  name="num_sessions"
+                  step="any"
+                  class="numberinput form-control"
+                />
               </div>
               <button class="btn btn-primary" type="submit">
                 Query Metadata
