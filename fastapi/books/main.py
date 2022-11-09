eb2ef279449347f07877c831c7f5705d22256e51
Arman <rmanaem@protonmail.ch>
2022-11-09 13:49:53 -0500

    Modified the API - Changed the BOOKS database to a list - Modified the post method to use a book object

--- fastapi/books/main.py
+++ fastapi/books/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
 from typing import Optional
-from models import Directions
+from models import Directions, Book
 
 app = FastAPI()
 
-BOOKS = {
-    'book_1': {'title': 'Title One', 'author': 'Author One'},
-    'book_2': {'title': 'Title Two', 'author': 'Author Two'},
-    'book_3': {'title': 'Title Three', 'author': 'Author Three'},
-    'book_4': {'title': 'Title Four', 'author': 'Author Four'},
-    'book_5': {'title': 'Title Five', 'author': 'Author Five'},
-}
+BOOKS = []
 
 @app.get("/")
 async def get_all_books():
@@ -40,16 +34,9 @@ async def skip_book(book_id: Optional[str]):
     return BOOKS
 
 @app.post("/")
-async def create_book(book_title, book_author):
-    current_book_id = 0
-    
-    if len(BOOKS) > 0:
-        for book in BOOKS:
-            x = int(book.split('_')[-1])
-            if x > current_book_id:
-                current_book_id = x
-    BOOKS[f'book_{current_book_id}']: {'title': book_title, 'author': book_author}
-    return BOOKS[f'book_{current_book_id}']
+async def create_book(book: Book):
+    BOOKS.append(book)
+    return book
 
 @app.put("/{book_id}")
 async def update_book(book_id: str, book_title: str, book_author: str):
