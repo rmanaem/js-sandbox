e9101263a4274626d9a4b8638e4588a106f50d74
Arman <rmanaem@protonmail.ch>
2022-11-09 14:18:49 -0500

    Made a pre-defined body within the request body for the post method

--- fastapi/books/models.py
+++ fastapi/books/models.py
@@ -15,3 +15,14 @@ class Book(BaseModel):
     author : str = Field(min_length=1, max_length=100)
     description : Optional[str] = Field(title="Description of the book", min_length=1, max_length=100)
     rating : int = Field(gt=-1, lt=101)
+
+    class Config:
+        schema_extra = {
+            "example" : {
+                "id" : "b365f3ec-82cb-440b-b77c-e4fe7dfffc25",
+                "title": "Computer Science Pro",
+                "author": "Codingwithroby",
+                "description": "A very nice description of a book",
+                "rating": 75
+            }
+        }
