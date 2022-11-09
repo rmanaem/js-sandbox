cd8930a82b1e6c3a8874d6df4e6b9583c0389455
Arman <rmanaem@protonmail.ch>
2022-11-09 13:42:23 -0500

    Implemented the book model

--- fastapi/books/models.py
+++ fastapi/books/models.py
@@ -1,7 +1,16 @@
 from enum import Enum
+from pydantic import BaseModel
+from uuid import UUID
 
 class Directions(str, Enum):
     north = "North"
     south = "South"
     east = "East"
     west = "West"
+
+class Book(BaseModel):
+    id : UUID
+    title : str
+    author : str
+    description : str
+    rating : int
