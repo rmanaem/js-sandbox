0dd0ede38b6ed78023d226edf48b6563ab830ee1
Arman <rmanaem@protonmail.ch>
2022-11-11 12:22:33 -0500

    Implemented update_todo method

--- fastapi/todo/main.py
+++ fastapi/todo/main.py
@@ -23,14 +23,14 @@ async def read_all(db: Session = Depends(get_db)):
 
 
 @app.get("/todo/{todo_id}")
-async def read_todo(todo_id: int, db:Session = Depends(get_db)):
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model:
         return todo_model
     raise not_found(todo_id)
 
 @app.post("/")
-async def create_todo(todo: Todo, db:Session = Depends(get_db)):
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
     todo_model = Todos()
     todo_model.title = todo.title
     todo_model.description = todo.description
@@ -44,3 +44,24 @@ async def create_todo(todo: Todo, db:Session = Depends(get_db)):
         "status" : 201,
         "transaction" : "Successful"
     }
+
+@app.put("/")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model:
+        todo_model.title = todo.title
+        todo_model.description = todo.description
+        todo_model.priority = todo.priority
+        todo_model.complete = todo.complete
+
+        db.add(todo_model)
+        db.commit()
+
+        return {
+            "status": 201,
+            "transaction" : "Successful"
+        }
+    
+    raise not_found(todo_id)
+    
+    
