ea9a28017c334a2dee898e0e69e0bb05c23c6e98
Arman <rmanaem@protonmail.ch>
2022-11-11 11:44:01 -0500

    Implemented read_todo method

--- fastapi/todo/main.py
+++ fastapi/todo/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import models
 from database import engine, SessionLocal
+from utils import not_found
 
 
 app = FastAPI()
@@ -19,3 +20,11 @@ def get_db():
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db:Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model:
+        return todo_model
+    raise not_found(todo_id)
