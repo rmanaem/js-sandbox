5d0ffc5a2f364a46c44b9d46370dd35d25180068
Arman <rmanaem@protonmail.ch>
2022-11-11 12:33:18 -0500

    Implemented successful_response method

--- fastapi/todo/utils.py
+++ fastapi/todo/utils.py
@@ -2,3 +2,9 @@ from fastapi import HTTPException
 
 def not_found(todo_id: int):
     return HTTPException(status_code=404, detail=f"Todo with id:{todo_id} was not found.")
+
+def successful_response(status_code: int):
+    return {
+        "status": status_code,
+        "transaction": "Successful"
+    }
