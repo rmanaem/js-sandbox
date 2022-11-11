da627d2b334c0c0003cf919225fa080e72d7ae1a
Arman <rmanaem@protonmail.ch>
2022-11-11 15:51:51 -0500

    Implemented the Users table and updated the Todos table

--- fastapi/todo/models.py
+++ fastapi/todo/models.py
@@ -1,8 +1,21 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from pydantic import BaseModel, Field
 from typing import Optional
 
 from database import Base
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hash_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todos", back_populates="owner")
 
 class Todos(Base):
     __tablename__ = "todos"
@@ -11,6 +24,9 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="todos")
 
 class Todo(BaseModel):
     title: str
