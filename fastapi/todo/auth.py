3effbb561bf2328472b917c1dd189bab0e11b37e
Arman <rmanaem@protonmail.ch>
2022-11-14 01:12:51 -0500

    Implemented jwt for authorization

--- fastapi/todo/auth.py
+++ fastapi/todo/auth.py
@@ -3,15 +3,21 @@ from pydantic import BaseModel
 from typing import Optional
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt
 
 from models import Users, User
 from database import SessionLocal, engine, Base
 
+SECRET_KEY = "hsafowhshfaownfoasfnouwtbg"
+ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
 app = FastAPI()
@@ -37,6 +43,21 @@ def authenticate_user(username: str, password: str, db):
     return False
 
 
+def create_access_token(username: str, user_id: int, expire_delta: Optional[timedelta] = None):
+    if expire_delta:
+        expire = datetime.utcnow() + expire_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    payload = {
+        "sub" : username,
+        "id" : user_id,
+        "exp" : expire
+        }
+
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+
 @app.post("/create/user")
 async def create_user(new_user: User, db: Session = Depends(get_db)):
     user_model = Users()
@@ -57,6 +78,6 @@ async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends(), d
     user = authenticate_user(form_data.username, form_data.password, db)
 
     if user:
-        return "User is validated."
+        return {"token" : create_access_token(user.username, user.id, expire_delta=timedelta(minutes=20))}
 
     raise HTTPException(status_code=404, detail="User not found.")
