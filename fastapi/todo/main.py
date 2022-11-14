560ac9bcddebf837f04e1f21cf3efabcc48dec37
Arman <rmanaem@protonmail.ch>
2022-11-14 14:10:49 -0500

    Included companyapis router in the main app

--- fastapi/todo/main.py
+++ fastapi/todo/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from models import Base
 from database import engine
 from routers import auth, todos
+from company import companyapis
 
 
 app = FastAPI()
@@ -11,3 +12,8 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(
+    companyapis.router, 
+    prefix="/companyapis", 
+    tags=["companyapis"], 
+    responses={418: {"description": "Internal use only"}})
