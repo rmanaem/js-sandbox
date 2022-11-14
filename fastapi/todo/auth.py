730e24c95f5c2cd920b954c7aa41e728d07bc377
Arman <rmanaem@protonmail.ch>
2022-11-14 01:26:12 -0500

    Implemented get_current_user method to validate a jwt

--- fastapi/todo/auth.py
+++ fastapi/todo/auth.py
@@ -5,7 +5,7 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 
 from models import Users, User
 from database import SessionLocal, engine, Base
@@ -58,6 +58,18 @@ def create_access_token(username: str, user_id: int, expire_delta: Optional[time
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 
+async def get_current_user(token: str = Depends(oauth2_bearer)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
 @app.post("/create/user")
 async def create_user(new_user: User, db: Session = Depends(get_db)):
     user_model = Users()
@@ -80,4 +92,4 @@ async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends(), d
     if user:
         return {"token" : create_access_token(user.username, user.id, expire_delta=timedelta(minutes=20))}
 
-    raise HTTPException(status_code=404, detail="User not found.")
+    raise HTTPException(status_code=404, detail="User not found")
