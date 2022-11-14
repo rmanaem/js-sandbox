108eb521b07bba83820d2ed50769960647a82edc
Arman <rmanaem@protonmail.ch>
2022-11-14 15:14:31 -0500

    Implemented user_password_change method

--- fastapi/todo/routers/users.py
+++ fastapi/todo/routers/users.py
@@ -2,11 +2,13 @@ import sys
 sys.path.append("..")
 
 from fastapi import Depends, APIRouter
-from models import Users
-from database import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from .auth import get_current_user, get_user_exception, get_password_hash, get_db
+
+from database import engine, SessionLocal, Base
+from models import Users, UserVerification
+from .auth import get_current_user, get_user_exception, get_password_hash, get_db, bcrypt_context
+from .todos import successful_response
 
 
 router = APIRouter(
@@ -40,3 +42,21 @@ async def user_by_query(user_id: int, db: Session = Depends(get_db)):
         return user_model
 
     return f"User id:{user_id} is invalid"
+
+
+@router.put("/user/password")
+async def user_password_change(user_verification: UserVerification, user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+    if user_model:
+        if user_verification.username == user_model.username and bcrypt_context.verify(user_verification.password, user_model.hash_password):
+            user_model.hash_password = get_password_hash(user_verification.new_password)
+
+            db.add(user_model)
+            db.commit()
+
+            return successful_response(204)
+    
+    return "Invalid user or request"
