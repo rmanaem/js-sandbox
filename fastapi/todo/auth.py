334897bf4a3fbff275c6e933509630fa7d976204
Arman <rmanaem@protonmail.ch>
2022-11-14 00:34:47 -0500

    Modified create_user method Calling the method will now create users and add them to user table in the database

--- fastapi/todo/auth.py
+++ fastapi/todo/auth.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional
-from models import Users, User
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+
+from models import Users, User
+from database import SessionLocal, engine, Base
+
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 def get_password_hash(password):
@@ -15,7 +28,7 @@ def get_password_hash(password):
 
 
 @app.post("/create/user")
-async def create_user(new_user: User):
+async def create_user(new_user: User, db: Session = Depends(get_db)):
     user_model = Users()
     user_model.email = new_user.email
     user_model.username = new_user.username
@@ -25,5 +38,6 @@ async def create_user(new_user: User):
     user_model.hash_password = get_password_hash(new_user.password)
     user_model.is_active = True
 
-    return user_model
+    db.add(user_model)
+    db.commit()
 
