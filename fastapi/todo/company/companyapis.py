17f2144cd7051d413d097227d55d397a38d81505
Arman <rmanaem@protonmail.ch>
2022-11-14 13:59:17 -0500

    Added company directory and companyapis module as mocks

---
+++ fastapi/todo/company/companyapis.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/")
+async def company_name():
+    return {"name": "Some Company, LLC"}
+
+
+@router.get("/employees")
+async def number_of_employees():
+    return 77
