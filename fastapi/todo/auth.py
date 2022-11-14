69e3b1885cb3e7f98183d15076d22dfdcf179d38
Arman <rmanaem@protonmail.ch>
2022-11-14 00:53:33 -0500

    Implemented user authentication - Implemented authenticate_user method - Implemented login_access_token method

--- fastapi/todo/auth.py
+++ fastapi/todo/auth.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
 from models import Users, User
 from database import SessionLocal, engine, Base
@@ -27,6 +28,15 @@ def get_password_hash(password):
     return bcrypt_context.hash(password)
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+
+    if user and bcrypt_context.verify(password, user.hash_password):
+        return user
+
+    return False
+
+
 @app.post("/create/user")
 async def create_user(new_user: User, db: Session = Depends(get_db)):
     user_model = Users()
@@ -41,3 +51,12 @@ async def create_user(new_user: User, db: Session = Depends(get_db)):
     db.add(user_model)
     db.commit()
 
+
+@app.post("/token")
+async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if user:
+        return "User is validated."
+
+    raise HTTPException(status_code=404, detail="User not found.")
